a8cf3eaa327167a6a5472e64a95fa4f31564b146
Tarida Hutahaean <tarida@gmail.com>
2020-06-02 22:31:18 +0700

    initial commit

---
+++ dist/precache-manifest.4780061a82b642faf7256bfdf28aacfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f360d2d438bf08390320",
+    "url": "/css/app.560b6e36.css"
+  },
+  {
+    "revision": "d95713b2a8297225a8816dfcc0c5a982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f360d2d438bf08390320",
+    "url": "/js/app.de1f9d3d.js"
+  },
+  {
+    "revision": "ba537731dfe6b69bf8ce",
+    "url": "/js/chunk-vendors.d28017df.js"
+  },
+  {
+    "revision": "7cd1ba918f39e339f9431abddbc1faa1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
